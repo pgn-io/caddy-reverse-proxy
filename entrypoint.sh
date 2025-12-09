883face3d2f92b1def5f2be75fe473769cf0f29b
Sascha <sascha@supercodebros.com>
2025-12-09 21:03:14 +0100

    chore(*): add vertical path to caddyfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,23 @@
 
 set -euo pipefail
 
-# for backwards compatibility, seperates host and port from url
+# for backwards compatibility, separates host and port from url
 export FRONTEND_DOMAIN=${FRONTEND_DOMAIN:-${FRONTEND_HOST%:*}}
 export FRONTEND_PORT=${FRONTEND_PORT:-${FRONTEND_HOST##*:}}
 
-export BACKEND_DOMAIN=${BACKEND_DOMAIN:-${BACKEND_HOST%:*}}
-export BACKEND_PORT=${BACKEND_PORT:-${BACKEND_HOST##*:}}
+export IDENTITY_SERVICE_DOMAIN=${IDENTITY_SERVICE_DOMAIN:-${IDENTITY_SERVICE_HOST%:*}}
+export IDENTITY_SERVICE_PORT=${IDENTITY_SERVICE_PORT:-${IDENTITY_SERVICE_HOST##*:}}
+
+export DATA_BRIDGE_DOMAIN=${DATA_BRIDGE_DOMAIN:-${DATA_BRIDGE_HOST%:*}}
+export DATA_BRIDGE_PORT=${DATA_BRIDGE_PORT:-${DATA_BRIDGE_HOST##*:}}
 
 # strip https:// or https:// from domain if necessary
 FRONTEND_DOMAIN=${FRONTEND_DOMAIN##*://}
-BACKEND_DOMAIN=${BACKEND_DOMAIN##*://}
+IDENTITY_SERVICE_DOMAIN=${IDENTITY_SERVICE_DOMAIN##*://}
+DATA_BRIDGE_DOMAIN=${DATA_BRIDGE_DOMAIN##*://}
 
 echo using frontend: ${FRONTEND_DOMAIN} with port: ${FRONTEND_PORT}
-echo using backend: ${BACKEND_DOMAIN} with port: ${BACKEND_PORT}
+echo using identity-service: ${IDENTITY_SERVICE_DOMAIN} with port: ${IDENTITY_SERVICE_PORT}
+echo using data-bridge: ${DATA_BRIDGE_DOMAIN} with port: ${DATA_BRIDGE_PORT}
 
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
